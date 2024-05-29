1e8ea15beb56d45adca8d3a63088bcc4f62c37f5
busra <kocarslanbusrab@gmail.com>
2024-05-29 21:30:01 +0300

    pagination disabled

--- src/pages/Dasboard.jsx
+++ src/pages/Dasboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useBlogRequest from "../services/useBlogRequest";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "../component/blog/Card";
@@ -6,19 +6,24 @@ import { setCurrentPage } from "../features/blogSlice";
 import { Grid, Pagination, Stack } from "@mui/material";
 
 const Dashboard = () => {
-  const { getBlogs, getUsers,getBlogUser } = useBlogRequest();
+  const { getBlogs, getUsers, getBlogUser } = useBlogRequest();
   const dispatch = useDispatch();
   const { blog, currentPage, totalPage } = useSelector((state) => state.blogs);
 
+  const [isDisabled, setIsDisabled] = useState(false); // Pagination butonlarını devre dışı bırakmak için state
+
   useEffect(() => {
     getBlogs(currentPage);
-    // getBlogUser()
     getUsers();
-    
   }, [currentPage]); // Sayfa değişiminde blogları ve kullanıcıları tekrar getir
 
   const handlePageChange = (event, value) => {
     dispatch(setCurrentPage(value));
+    setIsDisabled(true); // Butonları devre dışı bırak
+
+    setTimeout(() => {
+      setIsDisabled(false); // 2 saniye sonra butonları etkinleştir
+    }, 2500);
   };
 
   return (
@@ -37,7 +42,7 @@ const Dashboard = () => {
           page={currentPage}
           onChange={handlePageChange}
           variant="outlined"
-          
+          disabled={isDisabled} // Pagination bileşeninin devre dışı bırakılması
         />
       </Stack>
       <div style={{ paddingBottom: '100px' }}></div>
